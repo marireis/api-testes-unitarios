977bb5b24db7e720fe7a065798027264e11bd4c7
Marina Reis <marinareis.mr@gmail.com>
2023-01-28 17:08:07 -0300

    classe userdto

---
+++ src/main/java/com/testeunitarios/api/model/dto/UserDto.java
@@ -0,0 +1,23 @@
+package com.testeunitarios.api.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
